6568ff2e2206a4323a3d0c945b9c132ee182122a
vikash1311 <151333397+vikash1311@users.noreply.github.com>
2025-08-22 22:38:53 +0530

    Create Stock.java

---
+++ src/Stock.java
@@ -0,0 +1,26 @@
+public class Stock {
+    private String symbol;
+    private double price;
+
+    public Stock(String symbol, double price) {
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return symbol + " - $" + price;
+    }
+}
